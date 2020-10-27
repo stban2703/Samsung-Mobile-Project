e2f43afac72e21679eb6372e1692a44405a79df1
stban2703 <s.t.ban1401@gmail.com>
2020-10-27 18:19:39 -0500

    Added product screen

--- bin/scripts/main.js
+++ bin/scripts/main.js
@@ -10,8 +10,9 @@ function renderProducts(list) {
     productListTotal.innerHTML = `Total de productos mostrados: <strong>${list.length}</strong>`;
 
     list.forEach(function (elem) {
-        const newProduct = document.createElement('article');
+        const newProduct = document.createElement('a');
         newProduct.classList.add('productList__product');
+        newProduct.href = `product.html#${elem.id}`
 
         // Format price to money
         const formattedPrice = new Intl.NumberFormat().format(elem.price);
