13574d902f8e984913ea175fbf2d40ec700835b1
stban2703 <s.t.ban1401@gmail.com>
2020-11-02 23:36:48 -0500

    Separated js for custom input file

--- bin/scripts/createProduct.js
+++ bin/scripts/createProduct.js
@@ -1,40 +1,7 @@
 const createForm = document.querySelector('.form--create');
 const customInputFiles = createForm.querySelectorAll('.customFileInput');
 
-customInputFiles.forEach(function (e, i) {
-    const fileInput = e.querySelector('.customFileInput__file');
-    const fileName = e.querySelector('.customFileInput__title');
-    const fileImage = e.querySelector('.customFileInput__thumb');
-    const addBtn = e.querySelector('.customFileInput__add');
-    const removeBtn = e.querySelector('.customFileInput__remove');
-
-    fileInput.addEventListener('change', function () {
-        console.log(fileInput.files.length > 0 || fileInput.value != '')
-        if (fileInput.files.length > 0) {
-            fileName.innerText = `Image${i + 1}`;
-            let imageUrl = URL.createObjectURL(fileInput.files[0]);
-            fileImage.src = imageUrl;
-            fileImage.classList.remove('hidden');
-            addBtn.classList.add('hidden');
-            removeBtn.classList.remove('hidden');
-        } else {
-            addBtn.classList.remove('hidden');
-            removeBtn.classList.add('hidden');
-            fileName.innerText = 'Sube o arrastra una imagen';
-            fileImage.src = '';
-        }
-    })
-
-    removeBtn.addEventListener('click', function () {
-        addBtn.classList.remove('hidden');
-        removeBtn.classList.add('hidden');
-        fileInput.value = '';
-        fileName.innerText = 'Sube o arrastra una imagen';
-        fileImage.src = '';
-        console.log(fileInput.value)
-    })
-})
-
+setCustomInputFile(customInputFiles);
 setStars(createForm);
 
 createForm.addEventListener('submit', function (event) {
@@ -53,8 +20,7 @@ createForm.addEventListener('submit', function (event) {
             description: createForm.description.value,
             imageRef: ""
         };
-        console.log(newProduct);
-
+        
         productsRef // referencia de la colección
             .add(newProduct) // cree un nuevo elemento en la colección
             .then(function (docRef) {
