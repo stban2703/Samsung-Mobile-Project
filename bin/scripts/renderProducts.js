9c160e69dd88ffb0811d953c9b705c3b055a5480
stban2703 <s.t.ban1401@gmail.com>
2020-11-09 08:49:04 -0500

    Added filter and sort controls

--- bin/scripts/renderProducts.js
+++ bin/scripts/renderProducts.js
@@ -6,6 +6,7 @@ const sortForm = productListForm.sort;
 const filterClass = productListForm.class;
 const filterStorage = productListForm.storage;
 const filterCamera = productListForm.camera;
+const customAlert = document.querySelector('.customAlert');
 
 // Render products
 function renderProducts(list) {
@@ -50,90 +51,144 @@ function renderProducts(list) {
             </div>
             </a>
             <div class="productList__settings">
-                <a><img class="productList__option delete" src="./src/icons/delete.svg" alt="Borrar producto"></a>
+                <a><img class="productList__option" src="./src/icons/delete.svg" alt="Borrar producto"></a>
                 <a><img class="productList__option" src="./src/icons/addtocart.svg" alt="Agregar al carrito"></a>
                 <a href=${editUrl}><img class="productList__option" src="./src/icons/edit.svg" alt="Editar producto"></a>
             </div>`;
 
             loadStars(elem, newProduct);
 
-            const deleteBtn = newProduct.querySelector('.delete');
-            deleteBtn.addEventListener('click', function () {
-                //loader.classList.add('loader--show');
-                productsRef // referencia de la colección
-                    .doc(elem.id) // referencia de un documento específico en esa colección
-                    .delete() // elimine el documento asociado a esa referencia
-                    .then(function () {
-                        // debería entrar si todo sale bien
-                        console.log("Document successfully deleted!");
-                        getProducts('none'); // traiga los productos cuando estemos seguros de que ya eliminó el que le dijimos
-                    })
-                    .catch(function (error) {
-                        // debería entrar si ocurre algún error
-                        console.error("Error removing document: ", error);
-                    });
+            const deleteOption = newProduct.querySelectorAll('.productList__option');
+
+            deleteOption[0].addEventListener('click', function () {
+                customAlert.classList.remove('hidden');
+                handleDelete(elem.id);
             });
 
         });
-
         productListView.appendChild(newProduct);
-
     })
-
 }
 
-function getProducts(sort) {
+let objectList = [];
+function getProducts() {
     const loader = document.querySelector('.lds-ring');
     loader.classList.remove('hidden');
     productsRef  // referencia de la colección
         .get() // pide todos los documentos de la colección
         .then((querySnapshot) => {
-            const objects = [];
+            objectList = [];
             querySnapshot.forEach((doc) => {
                 const obj = doc.data();
                 obj.id = doc.id;
-                objects.push(obj);
+                objectList.push(obj);
                 //console.log(`${doc.id} => ${doc.data()}`);
             });
 
-            let filteredList = objects
             loader.classList.add('hidden');
             productList.classList.remove('hidden');
-
-            switch (sort) {
-                case "none":
-                    filteredList.sort(sortDefault);
-                    renderProducts(filteredList);
-                    break;
-
-                case "lowerPrice":
-                    filteredList.sort(sortByLowerPrice);
-                    renderProducts(filteredList);
-                    break;
-
-                case "higherPrice":
-                    filteredList.sort(sortByHigherPrice);
-                    renderProducts(filteredList);
-                    break;
-
-                case "betterRate":
-                    filteredList.sort(sortByBetterRate);
-                    renderProducts(filteredList);
-                    break;
-
-                case "lowerRate":
-                    filteredList.sort(sortByLowerRate);
-                    renderProducts(filteredList);
-                    break;
-            }
+            renderProducts(objectList);
         });
 }
 
-productListForm.addEventListener('change', function () {
+productListForm.addEventListener('input', function () {
+    let copy = objectList.slice();
     let newSort = sortForm.value;
+    let newClass = filterClass.value;
+    let newStorage = filterStorage.value;
+    let newCamera = filterCamera.value;
 
-    getProducts(newSort);
+    console.log(newSort)
+    switch (newSort) {
+        case "lowerPrice":
+            copy.sort(function (a, b) {
+                return a.price - b.price;
+            })
+            break;
+        case "higherPrice":
+            copy.sort(function (a, b) {
+                return b.price - a.price;
+            })
+            break;
+        case "lowerRate":
+            copy.sort(function (a, b) {
+                return a.rate - b.rate;
+            })
+            break;
+        case "betterRate":
+            copy.sort(function (a, b) {
+                return b.rate - a.rate;
+            })
+            break;
+    }
+
+
+    if (newClass) {
+        {
+            copy = copy.filter(function (elem) {
+                if (elem.class === newClass) {
+                    return true;
+                }
+            })
+        }
+    }
+
+    if (newStorage) {
+        {
+            copy = copy.filter(function (elem) {
+                if (elem.storage == parseInt(newStorage)) {
+                    return true;
+                }
+            })
+        }
+    }
+
+    if (newCamera) {
+        {
+            let range = newCamera.split(',');
+            let min = parseInt(range[0]);
+            let max = parseInt(range[1]);
+
+            console.log(min + " " + max);
+
+            copy = copy.filter(function (elem) {
+                if (elem.camera >= min && elem.camera <= max) {
+                    return true;
+                }
+            })
+        }
+    }
+
+    renderProducts(copy)
+
 })
 
 // render inicial con todos los productos
-getProducts("none");
+getProducts();
+
+function handleDelete(id) {
+
+    const deleteBtn = customAlert.querySelector('.customAlert__delete');
+    const cancelBtn = customAlert.querySelector('.customAlert__cancel');
+
+    deleteBtn.addEventListener('click', function () {
+        productsRef // referencia de la colección
+            .doc(id) // referencia de un documento específico en esa colección
+            .delete() // elimine el documento asociado a esa referencia
+            .then(function () {
+                // debería entrar si todo sale bien
+                console.log("Document successfully deleted!");
+                getProducts('none'); // traiga los productos cuando estemos seguros de que ya eliminó el que le dijimos
+                customAlert.classList.add('hidden');
+            })
+            .catch(function (error) {
+                // debería entrar si ocurre algún error
+                console.error("Error removing document: ", error);
+                customAlert.classList.add('hidden');
+            });
+    });
+
+    cancelBtn.addEventListener('click', function () {
+        customAlert.classList.add('hidden');
+    })
+}
